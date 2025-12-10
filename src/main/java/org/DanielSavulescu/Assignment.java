368e103264ce5a756f107569afae0c8210e91962
Daniel Savulescu <2540408@edu.vaniercollege.qc.ca>
2025-12-10 10:12:59 -0500

    Almost finished the Assignment class, might add some changes later on

--- src/main/java/org/DanielSavulescu/Assignment.java
+++ src/main/java/org/DanielSavulescu/Assignment.java
@@ -1,6 +1,7 @@
 package org.DanielSavulescu;
 
 import java.util.List;
+import java.util.Random;
 
 public class Assignment {
     private String assignmentId;
@@ -21,11 +22,46 @@ public class Assignment {
 
         double sum = 0;
         int count = 0;
-        for (Integer s : scores) {
-            sum += s;
+        for (Integer score : scores) {
+            sum += score;
             count++;
         }
 
         return sum / count;
+    }
+
+    /**
+     * generates random scores for all the students in an assignment
+     */
+    public void generateRandomScore() {
+        Random random = new Random();
+
+        for (int i = 0; i < scores.size(); i++) {
+            int randomNum = random.nextInt(11);
+            int score = 0;
+
+            if (randomNum == 0) {
+                score += random.nextInt(60);
+            } else if (randomNum == 1 || randomNum == 2) {
+                score += random.nextInt(60, 70);
+            } else if (randomNum == 3 || randomNum == 4) {
+                score += random.nextInt(70, 80);
+            } else if (randomNum == 5 || randomNum == 6 || randomNum == 7 || randomNum == 8) {
+                score += random.nextInt(80, 90);
+            } else {
+                score += random.nextInt(90, 101);
+            }
+
+            scores.set(i, score);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "assignmentId='" + assignmentId + '\'' +
+                ", assignmentName='" + assignmentName + '\'' +
+                ", weight=" + weight +
+                '}';
     }
 }
