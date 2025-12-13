fc4a8b561cf033a4a224d6496e29477eb1df3b4d
Daniel Savulescu <2540408@edu.vaniercollege.qc.ca>
2025-12-13 17:10:05 -0500

    Started the Course class and wrote the fields in it

---
+++ src/main/java/org/DanielSavulescu/Course.java
@@ -0,0 +1,14 @@
+package org.DanielSavulescu;
+
+import java.util.List;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private List<Assignment> assignments;
+    private List<Student> registeredStudents;
+
+    private static int nextId = 1;
+}
