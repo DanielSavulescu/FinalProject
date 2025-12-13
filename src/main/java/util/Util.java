8a758e7930b63986f99791112cc257213a91fb70
Daniel Savulescu <2540408@edu.vaniercollege.qc.ca>
2025-12-13 17:21:55 -0500

    Started the util class

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,24 @@
+package util;
+
+public class Util {
+
+    /**
+     * converts a string to title case
+     * @param str the original string
+     * @return the original string converted into title case
+     */
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        String[] words = str.split(" ");
+        String strToTitleCase = "";
+
+        for (String w : words) {
+
+        }
+
+        return strToTitleCase;
+    }
+}
